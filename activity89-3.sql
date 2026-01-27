4e2b2af8dd71173b72dd3834bfd5192af3c38ed7
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-01-26 21:34:53 -0800

    Fix SQL joins for correct data retrieval

--- activity89-3.sql
+++ activity89-3.sql
@@ -1,6 +1,6 @@
 SELECT Customers.CustomerName, Invoices.InvoiceID, SUM(InvoiceDetails.LineTotal) AS LineSum
 FROM Customers
-INNER JOIN Customers ON Invoices.CustomerID = Customers.CustomersID
-INNER JOIN Invoices ON InvoiceDetails.InvoiceID = Invoices.InvoiceID
+INNER JOIN Invoices ON Invoices.CustomerID = Customers.CustomersID
+INNER JOIN InvoiceDetails ON InvoiceDetails.InvoiceID = Invoices.InvoiceID
 GROUP BY Invoices.InvoiceID
 ORDER BY LineSum DESC;
