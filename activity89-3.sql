a494e88076abb5f500cbe239ccbfc144ecd2be0b
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-01-26 21:35:45 -0800

    Fix join condition for Customers table

--- activity89-3.sql
+++ activity89-3.sql
@@ -1,6 +1,6 @@
 SELECT Customers.CustomerName, Invoices.InvoiceID, SUM(InvoiceDetails.LineTotal) AS LineSum
 FROM Customers
-INNER JOIN Invoices ON Invoices.CustomerID = Customers.CustomersID
+INNER JOIN Invoices ON Invoices.CustomerID = Customers.CustomerID
 INNER JOIN InvoiceDetails ON InvoiceDetails.InvoiceID = Invoices.InvoiceID
 GROUP BY Invoices.InvoiceID
 ORDER BY LineSum DESC;
