893b6d3f0a24d524a74607d943bc5a034f373eb2
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-01-26 21:32:34 -0800

    Change ORDER BY clause to use ASC explicitly

--- activity89-2.sql
+++ activity89-2.sql
@@ -2,4 +2,4 @@ SELECT InvoiceDetails.InvoiceID, Products.ProductName, InvoiceDetails.UnitPrice
 FROM InvoiceDetails
 INNER JOIN Products ON InvoiceDetails.ProductNumber = Products.ProductNumber
 WHERE InvoiceDetails.InvoiceID = 3
-ORDER BY InvoiceDetails.UnitPrice;
+ORDER BY InvoiceDetails.UnitPrice ASC;
