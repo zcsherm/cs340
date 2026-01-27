61c7491173a4aab6aed318153c9f6a95720e66dc
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-01-26 21:27:31 -0800

    Add SQL query to retrieve invoice details for InvoiceID 3

---
+++ activity89-2.sql
@@ -0,0 +1,5 @@
+SELECT InvoiceDetails.InvoiceID, Products.ProductName, InvoiceDetails.UnitPrice
+FROM InvoiceDetails
+INNER JOIN Products ON InvoiceDetails.ProductNumber = Products.ProductNumber
+WHERE InvoiceDetails.InvoiceID = 3
+ORDER BY InvoiceDetails.UnitPrice;
