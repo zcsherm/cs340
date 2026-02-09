4a888af773829eb941834c38a47df05491e01206
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-02-09 15:20:18 -0800

    Add files via upload

--- query1.sql
+++ query1.sql
@@ -1,38 +1,9 @@
--- Write the query to create the 4 tables below.
+-- Find all films with maximum length or minimum replacement cost (compared to all other % films).
+-- In other words let L be the maximum film length, and let R be the minimum replacement cost in the table film. 
+-- You need to find all films that have length L or replacement cost R or both length L and replacement cost R.
+-- If a film has either a maximum length OR a minimum replacement cost it should appear in the result set. 
+-- It does not need to have both the maximum length AND minimum replacement cost.
+-- You just need to return the film_id for this query.
+-- Order your results by film_id in ascending order.
 
-CREATE TABLE client (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  first_name varchar(255) NOT NULL,
-  last_name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  CONSTRAINT full_name UNIQUE (first_name, last_name),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-    start_date date NOT NULL,
-    email varchar(255), NOT NULL,
-    CONSTRAINT full_name UNIQUE (first_name, last_name),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE project (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    title varchar(255) NOT NULL UNIQUE,
-    comments TEXT,
-    cid int(11),
-    FOREIGN KEY (cid) REFERENCES client(id),
-    PRIMARY KEY id
-);
-
-CREATE TABLE works_on (
-    pid int(11),
-    eid int(11),
-    due_date date NOT NULL,
-    FOREIGN KEY (pid) REFERENCES project(id),
-    FOREIGN KEY (eid) REFERENCES employee(id),
-    PRIMARY KEY (pid, eid)
-);
+-- Put query for Q1 here
---
+++ query5.sql
@@ -0,0 +1,6 @@
+-- Find the film_title of all films which feature both UMA WOOD and FRED COSTNER
+-- Order the results by film_title in descending order.
+--  Warning: this is a tricky one and while the syntax is all things you know, you have to think a bit oustide the box 
+--  to figure out how to get a table that shows pairs of actors in movies.
+
+-- Put your query for Q5 here.
