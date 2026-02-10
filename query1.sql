ab56acb9de55496bd43c5f6cce6ef7ee9a7383b6
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2026-02-09 20:58:28 -0800

    Implement query to fetch film titles by criteria
    Add SQL query to select film titles based on length and replacement cost.

--- query1.sql
+++ query1.sql
@@ -7,3 +7,11 @@
 -- Order your results by film_id in ascending order.
 
 -- Put query for Q1 here
+SELECT title FROM film WHERE
+(
+film.length = SELECT MAX(length) FROM film
+)
+  OR
+(
+film.replacement_cost = SELECT MIN(film.replacement_cost) FROM film
+);
